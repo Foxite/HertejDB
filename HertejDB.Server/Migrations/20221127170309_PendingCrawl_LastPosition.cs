bc4b5e0566a640b7640c01f6d704980be4661557
Dirk Kok <the@dirkkok.nl>
2022-11-27 18:03:29 +0100

    db migration for last commit

---
+++ HertejDB.Server/Migrations/20221127170309_PendingCrawl_LastPosition.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HertejDB.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingCrawlLastPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastPosition",
+                table: "PendingCrawls",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPosition",
+                table: "PendingCrawls");
+        }
+    }
+}
